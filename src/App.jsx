a2f13cf3a75d1e0de5932e3ffe40d02b1cfe99b7
dangvinh77 <phamdangvinh2002@gmail.com>
2025-03-10 20:12:16 +0700

    Semi-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import HomePage from "./pages/HomePage";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Disclaimer from "./pages/AboutUs/Disclaimer";
+import PrivacyPolicy from "./pages/AboutUs/PrivacyPolicy";
+import TicketsTerm from "./pages/AboutUs/TicketsTerm";
+import Gallery from "./pages/Gallery/Gallery";
+import Websitegallery from "./pages/Gallery/Websitegallery";
+import Visitorgallery from "./pages/Gallery/Visitorgallery";
+import Activity from "./pages/Activity/Activity";
+import Accessibility from "./pages/Activity/Accessibility";
+import Attraction from "./pages/Activity/Attractions";
+import Beach from "./pages/Activity/Beach";
+import Events from "./pages/Activity/Events";
+import FishingPier from "./pages/Activity/FishingPier";
+import FoodNDrink from "./pages/Activity/FoodNDrink";
+import Waterpark from "./pages/Activity/Waterpark";
+import Tickets from "./pages/Tickets/Tickets";
+import ContactUs from "./pages/ContactUs";
+import Review from "./pages/Review";
+import Notfound from "./pages/Notfound";
+import Layout from "./layouts/Layout";
+import "./styles/App.scss";
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/aboutus/disclaimer" element={<Disclaimer />} />
+            <Route path="/aboutus/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/aboutus/tickets-term" element={<TicketsTerm />} />
+            <Route path="/gallery/visitor" element={<Visitorgallery />} />
+            <Route path="/gallery/website" element={<Websitegallery />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/activity" element={<Activity />} />
+            <Route path="/activity/accessibility" element={<Accessibility />} />
+            <Route path="/activity/attractions" element={<Attraction />} />
+            <Route path="/activity/beach" element={<Beach />} />
+            <Route path="/activity/events" element={<Events />} />
+            <Route path="/activity/fishingpier" element={<FishingPier />} />
+            <Route path="/activity/foodndrink" element={<FoodNDrink />} />
+            <Route path="/activity/waterpark" element={<Waterpark />} />
+            <Route path="/tickets" element={<Tickets />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/review" element={<Review />} />
+            <Route path="*" element={<Notfound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
